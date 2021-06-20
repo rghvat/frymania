f05efd1852f9ed952324c121460dd0666c6a65b7
raghav atreya <raghavpushkalatreya@gmail.com>
2021-06-20 19:12:52 +0530

    Initial Commit

--- fman/asgi.py
+++ fman/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fryman project.
+ASGI config for fman project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fryman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fman.settings')
 
 application = get_asgi_application()
--- fman/wsgi.py
+++ fman/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fryman project.
+WSGI config for fman project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fryman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fman.settings')
 
 application = get_wsgi_application()
