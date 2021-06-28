9f99f39161bb3a0a49133537eabf78c076674ff0
raghav atreya <raghavpushkalatreya@gmail.com>
2021-06-28 09:33:53 +0530

    remove django heroku setting

--- fman/settings.py
+++ fman/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,5 +138,3 @@ MEDIA_URL  = '/media/'
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
